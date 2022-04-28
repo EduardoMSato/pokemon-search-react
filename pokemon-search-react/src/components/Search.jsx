9a8f758f3a209093b51161fa40ea6eb4c66cf87c
Eduardo Minakawa Sato <eduardo.m.satog@gmail.com>
2022-04-28 17:36:16 -0300

    Criado pasta Services e arquivo getPokemon.js, ao qual, acessa a API e retorna dados relacionado a busca do campo pokemon

--- pokemon-search-react/src/components/Search.jsx
+++ pokemon-search-react/src/components/Search.jsx
@@ -2,16 +2,21 @@ import React from 'react';
 import { Form, Container, Col, Button, FormControl} from 'react-bootstrap';
 
 export default function Search(props){
+
+    const [search, setSearch] = React.useState('');
+
     return (
         <Container>
+            <h1>{search}</h1>
             <Form className="mt-2">
 
                 <Col sm={10} className="my-1">
                     <Form.Control
+                        onChange={(e) => setSearch(e.target.value)}
                         placeholder="Search for Pokemon"/>
                 </Col>
                 <Col sm={2} className="my-1">
-                    <Button block>Search</Button>
+                    <Button block onClick={(e) => props.getPokemon(search)}>Search</Button>
                 </Col>
 
             </Form>
--- pokemon-search-react/src/pages/home.jsx
+++ pokemon-search-react/src/pages/home.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
 import Search from '../components/Search';
+import { fetchPokemon } from '../services/getPokemon';
 
 export default function HomePage(){
+
+    const [pokemon, setPokemon] = React.useState();
+
+    const getPokemon = async (query) =>{
+        const response = await fetchPokemon(query);
+        const results = await response.json();
+        console.log(results);
+    }
+
     return (
         <div>
-            <Search />
+            <Search getPokemon={getPokemon}/>
         </div>
     )
 }
