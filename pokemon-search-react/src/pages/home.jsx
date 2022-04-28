36c585c8a6259670c288e2c3703ded0ef2169704
Eduardo Minakawa Sato <eduardo.m.satog@gmail.com>
2022-04-28 17:46:26 -0300

    Na home.jsx, criado variaveis para a leitura e renderizacao condicional

--- pokemon-search-react/src/pages/home.jsx
+++ pokemon-search-react/src/pages/home.jsx
@@ -5,16 +5,24 @@ import { fetchPokemon } from '../services/getPokemon';
 export default function HomePage(){
 
     const [pokemon, setPokemon] = React.useState();
+    const [loading, setLoading] = React.useState(false);
 
     const getPokemon = async (query) =>{
+        setLoading(true);
         const response = await fetchPokemon(query);
         const results = await response.json();
-        console.log(results);
+        setPokemon(results);
+        setLoading(false);
     }
 
     return (
         <div>
             <Search getPokemon={getPokemon}/>
+            {!loading && pokemon ?(
+                <div>
+                    <h1>{pokemon.name}</h1>
+                </div>
+            ): null}
         </div>
     )
 }
